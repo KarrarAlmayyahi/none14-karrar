d87a43ba67d94a0302d98e440d9783a1f63a5a81
mahdi8ali <attaba5005@gmail.com>
2023-08-16 23:56:37 +0300

    Update main.dart
    s

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_3/view/home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/route_manager.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
